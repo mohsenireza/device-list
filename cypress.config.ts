633b1fdbeae5abeb2fa57e90c7c0c08cf1b8f3f2
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2023-04-13 07:46:03 +0330

    Update e2e script

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:8080',
         retries: 2,
+        video: false,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
